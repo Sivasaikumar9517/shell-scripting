3f2dc877fa9da0fa94a88c71b805fdc386315673
sivasaikum <sivasaikumarallampati@gmail.com>
2025-12-01 21:47:10 +0530

    Conversation added

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Rahim
+PERSON2=Pavan
+
+echo " $PERSON1 : Hi $PERSON2 "
+echo " $PERSON2 : Hi $PERSON1 , How are you ? "
+
+echo " $PERSON1: I am good, How are you ? "
+echo " $PERSON2 : I am also fine, Thank you "
