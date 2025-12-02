767d4e3b748cc8d97263287b6550dfc2d34080b1
sivasaikum <sivasaikumarallampati@gmail.com>
2025-12-02 20:18:40 +0530

    datatypes

--- 08-installpackages.sh
+++ 08-installpackages.sh
@@ -6,7 +6,20 @@ if [ $USERID -ne 0 ]
 then
     echo " ERROR: User must have sudo access to run this script "
     exit 1
+else
+    dnf list installed mysql 
+    if [ $? -ne 0 ]
+    then
+        dnf install mysql -y
+        if [ $? -ne 0 ]
+        then
+            echo " Installation of Mysql is .... Successs"
+        else
+            echo " Installation of mysql is ..... Failure "
+        fi
+    else 
+        echo " Mysql is already installed"
+    fi
 fi
 
-dnf install mysqll -y
 
