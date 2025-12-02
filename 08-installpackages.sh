7ba1d15e306ec42825f2c0dd8b41ae264a0272c9
sivasaikum <sivasaikumarallampati@gmail.com>
2025-12-02 20:26:46 +0530

    datatypes

--- 08-installpackages.sh
+++ 08-installpackages.sh
@@ -23,4 +23,20 @@ else
     fi
 fi
 
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then 
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then 
+        echo " Installation of Git is .... Failure "
+        exit 1
+    else
+        echo " Installation of Git is .... Success"
+    fi
+else
+    echo " Git is already installed"
+fi
+
 
