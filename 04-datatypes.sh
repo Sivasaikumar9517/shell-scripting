ae6f846ce64f75ed4620332da01b742d4f9bc066
sivasaikum <sivasaikumarallampati@gmail.com>
2025-12-02 08:29:24 +0530

    datatypes

--- 04-datatypes.sh
+++ 04-datatypes.sh
@@ -2,10 +2,11 @@
 
 NUM1=$1
 NUM2=$2
+num3=$3
 
 TIMESTAMP=$(date)
 echo "Script executed at : $TIMESTAMP"
 
-SUM=$(($NUM1+$NUM2))
+SUM=$(($NUM1+$NUM2+$num3))
 
-echo "sum of $NUM1 and $NUM2 is : $SUM"
+echo "sum of $NUM1 , $NUM2 and $num3 is : $SUM"
