2405c2ea34fa3ba6aa1d97acc62c32a6f0aa594f
sivasaikum <sivasaikumarallampati@gmail.com>
2025-12-02 08:37:26 +0530

    datatypes

--- 05-arrays.sh
+++ 05-arrays.sh
@@ -7,4 +7,4 @@ echo "Second movie is : ${MOVIES[1]}"
 echo "Third movie is : ${MOVIES[2]}"
 echo "Fourth movie is : ${MOVIES[3]}"
 echo "All movie are : ${MOVIES[@]}" # print all the moviess
-echo "Length of movie list  : ${#MOVIES[@]}"
+echo "Length of movie list  : ${#MOVIES[@]}" # length of array 
