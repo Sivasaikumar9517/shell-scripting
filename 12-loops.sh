9c451d070f1f4ec9cb1131ecf4f67325f6049a55
sivasaikum <sivasaikumarallampati@gmail.com>
2025-12-03 16:37:59 +0530

    datatypes

---
+++ 12-loops.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+FILE_NAME=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$FILE_NAME-$TIME_STAMP.log"
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: User Must have root access to execute this script"
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+       echo " Installating of  $package is Failure " 
+       exit 1
+    else
+        echo " Installating of  $package is Success " 
+}
+
+for Package in $@
+do
+    dnf list installed $Package
+    if [ $? -ne 0 ]
+    then
+        dnf install $Package -y 
+        VALIDATE $Package "Installing $package is"
+    else
+        echo " $package is already Installed"
+    fi
+done
