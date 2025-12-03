d0d83838dc757fde6870adaafdbb90217c86e762
sivasaikum <sivasaikumarallampati@gmail.com>
2025-12-03 12:31:21 +0530

    datatypes

---
+++ 11.logs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+FILE_NAME=$( $0 )
+echo " $FILE_NAME"
+
+# USERID=$(id -u)
+# PACKAGE=$1
+# PACKAGE2=$2
+
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+
+# LOGS_FOLDER="/var/log/shellscript-logs"
+# FILE_NAME=$( $0 )
+
+
+
+# VALIDATE() {
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e " $2 ..... $R FAILURE"
+#         exit 1
+#     else
+#         echo -e " $2 ..... $G SUCCESS"
+#     fi
+# }
+
+# if [ $USERID -ne 0 ]
+# then
+#     echo "ERROR: User must have root privilages to execute this script "
+#     exit 1
+# fi 
+
+# dnf list installed $PACKAGE
+
+# if [ $? -ne 0 ]
+# then
+#     dnf install $PACKAGE -y
+#     VALIDATE $? "Installing $PACKAGE is "
+     
+# else
+#     echo -e " $PACKAGE Package is $Y Already  INSTALLED"
+# fi
+
+# dnf list installed $PACKAGE2
+
+# if [ $? -ne 0 ]
+# then
+#     dnf install $PACKAGE2 -y
+#     VALIDATE $? "Installing $PACKAGE2 is "
+     
+# else
+#     echo -e " $PACKAGE2 Package is $Y Already Installed"
+# fi
+
