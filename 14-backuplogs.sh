3af254eee5a3b166603354d73ff3acdb551ff7b9
sivasaikum <sivasaikumarallampati@gmail.com>
2025-12-05 11:26:58 +0530

    expense-mysql

--- 14-backuplogs.sh
+++ 14-backuplogs.sh
@@ -33,3 +33,32 @@ then
     echo -e " $R $DEST_DIR $N doesn't exists ... please check"
     exit 1
 fi
+
+echo "Script started executing at: $TIMESTAMP"
+
+FILE=$(find $LOGS_FOLDER -name "*.logs" -mtime +$DAYS)
+
+
+if [ -n "$FILE" ] # true if there are files to zip
+then
+    echo "FILES are : $FILE"
+    ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find $LOGS_FOLDER -name "*.logs" -mtime +$DAYS | zip -@ "$ZIP_FILE_NAME"
+
+    if [ -f "$ZIP_FILE_NAME" ]
+    then
+        echo -e " All the files older than $Y $DAYS $N are succesfully $G zipped $N "
+
+        while read -r filenames
+        do
+            echo -e " $R Deleting file are :$N $filenames "
+            rm -rf $filenames
+        done  <<< $FILE
+
+    else
+        echo -e " $R ERROR:: $N Failed to Create a $R ZIP FILE $N "
+        exit 1
+
+else
+    echo -e " No files found older than $Y $DAYS $N to  $G ZIP $N"
+fi 
