2830a8f92672c12760d18695d602f5bb1098bac1
sivasaikum <sivasaikumarallampati@gmail.com>
2026-04-18 14:19:16 +0530

    shell

--- prac.sh
+++ prac.sh
@@ -5,6 +5,15 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE_NAME="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p $LOGS_FOLDER
+
+echo " Script start executed at : $(date) " | tee -a $LOG_FILE_NAME
+
 
 
 if [ $USERID -ne 0 ]; then
@@ -21,31 +30,31 @@ VALIDATE(){
     fi
 }
 
-dnf list installed mysql
+dnf list installed mysql &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install mysql -y
+    dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? mysql
 else
-    echo -e " Mysql is Already Installed .... $Y Skipping $N"
+    echo -e " Mysql is Already Installed .... $Y Skipping $N" | tee -a $LOG_FILE_NAME
 fi
 
-dnf list installed nginx
+dnf list installed nginx &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install nginx -y
+    dnf install nginx -y &>>$LOG_FILE_NAME
     VALIDATE $? nginx
 else
-    echo -e " nginx is Already Installed ....  $Y Skipping $N"
+    echo -e " nginx is Already Installed ....  $Y Skipping $N" | tee -a $LOG_FILE_NAME
 fi
 
-dnf list installed gcc
+dnf list installed gcc &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install gcc -y
+    dnf install gcc -y &>>$LOG_FILE_NAME
     VALIDATE $? gcc
 else
-    echo -e " gcc is Already Installed .... $Y Skipping $N"
+    echo -e " gcc is Already Installed .... $Y Skipping $N" | tee -a $LOG_FILE_NAME
 fi
 
 
